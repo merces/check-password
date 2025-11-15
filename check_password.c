59c3785bf2f6765b1e2d4a05b6fa25228470700e
merces <nandu88@gmail.com>
2025-11-14 23:43:23 -0300

    clang-format

--- check_password.c
+++ check_password.c
@@ -17,318 +17,278 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#include <stdio.h>
-#include <string.h>
 #include <ctype.h>
-#include <time.h>
 #include <portable.h>
 #include <slap.h>
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
 
 #define MAX_LINE 60
 #define MAX_LOGFILE 250
 #define MAX_LOG_MSG 150
 
-struct config
-{
-   char log;
-   char logfile[MAX_LOGFILE];
+struct config {
+  char log;
+  char logfile[MAX_LOGFILE];
 } config;
 
 char msg[MAX_LOG_MSG];
 
-void logit(char s[])
-{
-	FILE *fp;
-	const time_t timer = time(NULL);
-	char *now = ctime(&timer);
+void logit(char s[]) {
+  FILE *fp;
+  const time_t timer = time(NULL);
+  char *now = ctime(&timer);
 
-	if (!config.log || config.logfile[0] == '\0')
-		return;
+  if (!config.log || config.logfile[0] == '\0')
+    return;
 
-	fp = fopen(config.logfile, "a");
+  fp = fopen(config.logfile, "a");
 
-	if (fp == NULL)
-		return;
+  if (fp == NULL)
+    return;
 
-	if (now)
-		now[strlen(now)-1] = '\0';	
+  if (now)
+    now[strlen(now) - 1] = '\0';
 
-	fprintf(fp, "error: %s - %s\n", now, s);
-	fclose(fp);
+  fprintf(fp, "error: %s - %s\n", now, s);
+  fclose(fp);
 }
 
-void loadconfig()
-{
-   FILE *fp;
-   char line[MAX_LINE];
-   char *param, *value;
-   int i;
+void loadconfig() {
+  FILE *fp;
+  char line[MAX_LINE];
+  char *param, *value;
+  int i;
 
 #ifdef CONF_FILE_PATH
-fp = fopen(CONF_FILE_PATH, "r");
+  fp = fopen(CONF_FILE_PATH, "r");
 #else
-fp = fopen("check_password.conf", "r");
+  fp = fopen("check_password.conf", "r");
 #endif
 
-   if (fp == NULL)
-      return;
+  if (fp == NULL)
+    return;
 
-	config.log = config.logfile[0] = 0;
+  config.log = config.logfile[0] = 0;
 
-   while (fgets(line, MAX_LINE, fp))
-   {
-      // comments
-      if (*line == '#')
-         continue;
+  while (fgets(line, MAX_LINE, fp)) {
+    // comments
+    if (*line == '#')
+      continue;
 
-      // remove newline
-      for (i=0; i<MAX_LINE; i++)
-      {   
-         if (line[i] == '\n' || i == MAX_LINE-1)
-         {   
-            line[i] = '\0';
-            break;
-         }
+    // remove newline
+    for (i = 0; i < MAX_LINE; i++) {
+      if (line[i] == '\n' || i == MAX_LINE - 1) {
+        line[i] = '\0';
+        break;
       }
+    }
 
-      param = strtok(line, "=");
-      value = strtok(NULL, "=");
+    param = strtok(line, "=");
+    value = strtok(NULL, "=");
 
-      if (param && (strcmp("log", param) == 0))
-      {
-         if (value && (strcmp("true", value) == 0))
-            config.log = 1;
+    if (param && (strcmp("log", param) == 0)) {
+      if (value && (strcmp("true", value) == 0))
+        config.log = 1;
+    } else if (param && (strcmp("logfile", param) == 0)) {
+      if (value)
+        strncpy(config.logfile, value, MAX_LOGFILE - 1);
+    }
+  }
+  fclose(fp);
+}
+
+static int basic_rules(const char *pw) {
+  char c = '\0';
+  int lower, upper, digit, size, i;
+
+  if (pw == NULL) {
+    if (config.log) {
+      snprintf(msg, MAX_LOG_MSG, "empty password");
+      logit(msg);
+    }
+    return 0;
+  }
+
+  lower = upper = digit = 0;
+  size = (int)strlen(pw);
+
+  if (size < 6) {
+    if (config.log) {
+      snprintf(msg, MAX_LOG_MSG, "password length (%d) is lower than 6", size);
+      logit(msg);
+    }
+    return 0;
+  }
+
+  if (size > 8) {
+    if (config.log) {
+      snprintf(msg, MAX_LOG_MSG, "password length (%d) is bigger than 8", size);
+      logit(msg);
+    }
+    return 0;
+  }
+
+  for (i = 0; i < size; i++) {
+    if (!isalnum(pw[i])) {
+      if (config.log) {
+        snprintf(msg, MAX_LOG_MSG, "character %c is not alpha-numeric", pw[i]);
+        logit(msg);
       }
-      else if (param && (strcmp("logfile", param) == 0))
-      {
-         if (value)
-            strncpy(config.logfile, value, MAX_LOGFILE);
+      return 0;
+    }
+    if (c == (char)tolower(pw[i])) {
+      if (config.log) {
+        snprintf(msg, MAX_LOG_MSG, "character %c repeated sequentially", pw[i]);
+        logit(msg);
       }
-   }
-	fclose(fp);
+      return 0;
+    }
+    if (islower(pw[i]))
+      lower++;
+    if (isupper(pw[i]))
+      upper++;
+    if (isdigit(pw[i]))
+      digit++;
+    c = tolower(pw[i]);
+  }
+
+  if (lower < 1) {
+    if (config.log) {
+      snprintf(msg, MAX_LOG_MSG, "no lower-case characters");
+      logit(msg);
+    }
+    return 0;
+  }
+  if (upper < 1) {
+    if (config.log) {
+      snprintf(msg, MAX_LOG_MSG, "no upper-case characters");
+      logit(msg);
+    }
+    return 0;
+  }
+  if (digit < 1) {
+    if (config.log) {
+      snprintf(msg, MAX_LOG_MSG, "no numbers");
+      logit(msg);
+    }
+    return 0;
+  }
+
+  if (strcasestr(pw, "password")) {
+    if (config.log) {
+      snprintf(msg, MAX_LOG_MSG, "password contains the word \"password\"");
+      logit(msg);
+    }
+    return 0;
+  }
+
+  return 1;
 }
 
-static int basic_rules(const char *pw)
-{
-	char c = '\0';
-	int lower, upper, digit, size, i;
+static int keyboard_seq(const char *pw) {
+  const char kbseq[] = "qwertyuiopasdfghjklzxcvbnm1234567890";
+  const char kbqes[] = "0987654321mnbvcxzlkjhgfdsapoiuytrewq";
+  char needle[4];
+  register int i, j;
 
-	if (pw == NULL)
-	{
-		if (config.log)
-		{
-			snprintf(msg, MAX_LOG_MSG, "empty password");
-			logit(msg);
-		}
-		return 0;
-	}
+  for (i = 0; i < (int)strlen(pw) - 2; i++) {
+    needle[0] = tolower(pw[i]);
+    needle[1] = tolower(pw[i + 1]);
+    needle[2] = tolower(pw[i + 2]);
+    needle[3] = '\0';
 
-	lower = upper = digit = 0;
-	size = (int) strlen(pw);
+    for (j = 0; j < (int)strlen(kbseq); j++) {
+      if (strstr(kbseq, needle) != NULL || strstr(kbqes, needle) != NULL) {
+        if (config.log) {
+          snprintf(msg, MAX_LOG_MSG, "password contains a keyboard sequence");
+          logit(msg);
+        }
+        return 0;
+      }
+    }
+  }
 
-	if (size < 6)
-	{
-		if (config.log)
-		{
-			snprintf(msg, MAX_LOG_MSG, "password length (%d) is lower than 6", size);
-			logit(msg);
-		}
-		return 0;
-	}
-
-	if (size > 8)
-	{
-		if (config.log)
-		{
-			snprintf(msg, MAX_LOG_MSG, "password length (%d) is bigger than 8", size);
-			logit(msg);
-		}
-		return 0;
-	}
-
-	for (i=0; i<size; i++)
-	{
-		if (! isalnum(pw[i]))
-		{
-			if (config.log)
-			{
-				snprintf(msg, MAX_LOG_MSG, "character %c is not alpha-numeric", pw[i]);
-				logit(msg);
-			}
-			return 0;
-		}
-		if (c == (char) tolower(pw[i]))
-		{
-			if (config.log)
-			{
-				snprintf(msg, MAX_LOG_MSG, "character %c repeated sequentially", pw[i]);
-				logit(msg);
-			}
-			return 0;
-		}
-		if (islower(pw[i])) lower++;
-		if (isupper(pw[i])) upper++;
-		if (isdigit(pw[i])) digit++;
-		c = tolower(pw[i]);
-	}
-
-	if (lower < 1)
-	{
-		if (config.log)
-		{
-			snprintf(msg, MAX_LOG_MSG, "no lower-case characters");
-			logit(msg);
-		}
-		return 0;
-	}
-	if (upper < 1)
-	{
-		if (config.log)
-		{
-			snprintf(msg, MAX_LOG_MSG, "no upper-case characters");
-			logit(msg);
-		}
-		return 0;
-	}
-	if (digit < 1)
-	{
-		if (config.log)
-		{
-			snprintf(msg, MAX_LOG_MSG, "no numbers");
-			logit(msg);
-		}
-		return 0;
-	}
-
-	if (strcasestr(pw, "password"))
-	{
-		if (config.log)
-		{
-			snprintf(msg, MAX_LOG_MSG, "password contains the word \"password\"");
-			logit(msg);
-		}
-		return 0;
-	}
-
-	return 1;
+  return 1;
 }
 
-static int keyboard_seq(const char *pw)
-{
-	const char kbseq[] = "qwertyuiopasdfghjklzxcvbnm1234567890";
-	const char kbqes[] = "0987654321mnbvcxzlkjhgfdsapoiuytrewq";
-	char needle[4];
-	register int i, j;
+static int checkdate(const char *pw) {
+  time_t now;
+  struct tm *d;
+  char year[] = "0000";
+  char mname[] = "Xxx";
+  char mnum[] = "00";
 
-	for (i=0; i < (int) strlen(pw)-2; i++)
-	{
-		needle[0] = tolower(pw[i]);
-		needle[1] = tolower(pw[i+1]);
-		needle[2] = tolower(pw[i+2]);
-		needle[3] = '\0';
+  (void)time(&now);
+  d = localtime(&now);
 
-		for (j=0; j < (int) strlen(kbseq); j++)
-		{
-			if (strstr(kbseq, needle) != NULL || strstr(kbqes, needle) != NULL)
-			{
-				if (config.log)
-				{
-					snprintf(msg, MAX_LOG_MSG, "password contains a keyboard sequence");
-					logit(msg);
-				}
-				return 0;
-			}
-		}
-	}
+  (void)strftime(year, 5, "%Y", d);
+  (void)strftime(mname, 4, "%b", d);
+  (void)strftime(mnum, 3, "%m", d);
 
-	return 1;
+  if (strstr(pw, year)) {
+    if (config.log) {
+      snprintf(msg, MAX_LOG_MSG, "password contains the current year (%s)",
+               year);
+      logit(msg);
+    }
+    return 0;
+  }
+  if (strcasestr(pw, mname)) {
+    if (config.log) {
+      snprintf(msg, MAX_LOG_MSG,
+               "password contains the current month name (%s)", mname);
+      logit(msg);
+    }
+    return 0;
+  }
+  if (strstr(pw, mnum)) {
+    if (config.log) {
+      snprintf(msg, MAX_LOG_MSG,
+               "password contains the current month number (%s)", mnum);
+      logit(msg);
+    }
+    return 0;
+  }
+
+  return 1;
 }
 
-static int checkdate(const char *pw)
-{
-	time_t now;
-	struct tm *d;
-	char year[] = "0000";
-	char mname[] = "Xxx";
-	char mnum[]= "00";
+static int checkusername(const char *dn, const char *pw) {
+  char *equal = strchr(dn, '=');
+  char nome[5];
+  register int i;
 
-	(void) time(&now);
-	d = localtime(&now);
+  if (!equal) {
+    if (config.log) {
+      snprintf(msg, MAX_LOG_MSG, "empty dn received");
+      logit(msg);
+    }
+    return 0;
+  }
 
-	(void) strftime(year, 5, "%Y", d);
-	(void) strftime(mname, 4, "%b", d);
-	(void) strftime(mnum, 3, "%m", d);
+  for (i = 0; i < 4; i++)
+    nome[i] = *(equal + i + 1);
+  nome[4] = '\0';
 
-	if (strstr(pw, year))
-	{
-		if (config.log)
-		{
-			snprintf(msg, MAX_LOG_MSG, "password contains the current year (%s)", year);
-			logit(msg);
-		}
-		return 0;
-	}
-	if (strcasestr(pw, mname))
-	{
-		if (config.log)
-		{
-			snprintf(msg, MAX_LOG_MSG, "password contains the current month name (%s)", mname);
-			logit(msg);
-		}
-		return 0;
-	}
-	if (strstr(pw, mnum)) 
-	{
-		if (config.log)
-		{
-			snprintf(msg, MAX_LOG_MSG, "password contains the current month number (%s)", mnum);
-			logit(msg);
-		}
-		return 0;
-	}
-
-	return 1;
+  if (strcasestr(pw, nome)) {
+    if (config.log) {
+      snprintf(msg, MAX_LOG_MSG, "password contains the username");
+      logit(msg);
+    }
+    return 0;
+  }
+  return 1;
 }
 
-static int checkusername(const char *dn, const char *pw)
-{
-	char *equal = strchr(dn, '=');
-	char nome[5];
-	register int i;
+int check_password(const char *pPasswd, char **ppErrStr __attribute__((unused)),
+                   const Entry *pEntry) {
+  loadconfig();
 
-	if (!equal)
-	{
-		if (config.log)
-		{
-			snprintf(msg, MAX_LOG_MSG, "empty dn received");
-			logit(msg);
-		}
-		return 0;
-	}
+  if (!basic_rules(pPasswd) || !keyboard_seq(pPasswd) || !checkdate(pPasswd) ||
+      !checkusername(pEntry->e_name.bv_val, pPasswd))
+    return 1;
 
-	for (i=0; i<4; i++)
-		nome[i] = *(equal+i+1);
-	nome[4] = '\0';
-
-	if (strcasestr(pw, nome))
-	{
-		if (config.log)
-		{
-			snprintf(msg, MAX_LOG_MSG, "password contains the username");
-			logit(msg);
-		}
-		return 0;
-	}
-	return 1;
-}
-
-int check_password(const char *pPasswd, char **ppErrStr __attribute__((unused)), const Entry *pEntry)
-{
-	loadconfig();
-
-	if (!basic_rules(pPasswd) || !keyboard_seq(pPasswd) ||
-	    !checkdate(pPasswd) || !checkusername(pEntry->e_name.bv_val, pPasswd))
-		return 1;
-
-	return 0;
+  return 0;
 }
